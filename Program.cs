fb64fab5d5912d1c92ec59cfac30dcbfd05dcaa7
bdf007 <bdf007@gmail.com>
2023-11-29 11:19:15 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace premier_programme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(  "Quel est ton nom? ");
+            string nomDeLaPersonne = Console.ReadLine();
+
+            Console.Write(  "Quek est votre age? ");
+            string ageDeLaPersonne = Console.ReadLine();
+            Console.WriteLine("Bonjour vous vous appelez " + nomDeLaPersonne + ", vous avez " + ageDeLaPersonne + " ans.");
+        }
+    }
+}
+
